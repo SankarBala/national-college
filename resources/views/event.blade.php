a0238417f2f5b1dcea5b30b28fb8ff75423ee50b
Sankar Bala <sankarbala232@gmail.com>
2022-01-23 19:32:15 +0600

    something added

---
+++ resources/views/event.blade.php
@@ -0,0 +1,464 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Mirrored from themes.themewild.com/edulife/event-single.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 23 Jan 2022 08:39:32 GMT -->
+<head>
+
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="keywords" content="" />
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<title>Edulife - Education & Courses Bootstrap 5 HTML5 Template</title>
+
+<link rel="icon" type="image/png" href="assets/img/favicon.png">
+
+<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900" rel="stylesheet">
+
+<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+<link rel="stylesheet" href="assets/css/fontawesome.min.css">
+
+<link rel="stylesheet" href="assets/css/jquery.fancybox.min.css">
+
+<link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+
+<link rel="stylesheet" href="assets/css/animate.min.css">
+
+<link rel="stylesheet" href="assets/css/slicknav.min.css">
+
+<link rel="stylesheet" href="assets/css/magnific-popup.css">
+
+<link rel="stylesheet" href="assets/css/default.css">
+<link rel="stylesheet" href="assets/css/style.css">
+<link rel="stylesheet" href="assets/css/responsive.css">
+</head>
+<body>
+
+<div class="preloader">
+<div class="loader">
+<div class="loader-page"></div>
+<div class="loader-page"></div>
+<div class="loader-page"></div>
+</div>
+</div>
+
+
+<header class="header">
+
+<div class="topbar">
+<div class="container">
+<div class="row">
+<div class="col-lg-8 col-12">
+
+<ul class="content">
+<li><i class="far fa-phone"></i> (+1) 123 456 7894 </li>
+<li><a href="https://themes.themewild.com/cdn-cgi/l/email-protection#6e070008012e0b160f031e020b400d0103"><i class="far fa-envelope"></i> <span class="__cf_email__" data-cfemail="3d54535b527d58455c504d5158135e5250">[email&#160;protected]</span> </a></li>
+<li><i class="far fa-clock"></i>Office Hour: 08:00AM - 06:00PM</li>
+</ul>
+
+</div>
+<div class="col-lg-4 col-12">
+<div class="topbar-right">
+
+<ul class="social">
+<li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+<li><a href="#"><i class="fab fa-twitter"></i></a></li>
+<li><a href="#"><i class="fab fa-instagram"></i></a></li>
+<li><a href="#"><i class="fab fa-whatsapp"></i></a></li>
+<li><a href="#"><i class="fab fa-youtube"></i></a></li>
+</ul>
+
+
+<div class="search">
+<ul>
+<li class="search_btn"><a href="#"><i class="far fa-search"></i></a></li>
+</ul>
+<div class="header_search_content">
+<div class="header_search_content_inner">
+<a href="#" class="close_btn"><i class="fal fa-times"></i></a>
+<form id="searchbox" method="get" action="#">
+<input class="search_query" type="text" id="search_query_top" name="s" placeholder="Enter Keyword" value="">
+<button type="submit" class="btn close-search"><i class="fal fa-search"></i></button>
+</form>
+</div>
+</div>
+</div>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+<div class="header-inner">
+<div class="container">
+<div class="row">
+<div class="col-lg-3 col-md-3 col-12">
+<div class="logo">
+<a href="index-2.html"><img src="assets/img/logo.png" alt="#"></a>
+</div>
+<div class="mobile-menu"></div>
+</div>
+<div class="col-lg-9 col-md-9 col-12">
+
+ <div class="header-widget">
+<div class="single-widget">
+<i class="far fa-phone"></i>
+<h4>Call Now<span>(+1) 123 456 7894</span></h4>
+</div>
+<div class="single-widget">
+<i class="far fa-envelope"></i>
+<h4>Email Us<a href="https://themes.themewild.com/cdn-cgi/l/email-protection#f891969e97b89d80999588949dd69b9795"><span><span class="__cf_email__" data-cfemail="c4adaaa2ab84a1bca5a9b4a8a1eaa7aba9">[email&#160;protected]</span></span></a></h4>
+</div>
+<div class="single-widget">
+<i class="far fa-map-marker-alt"></i>
+<h4>Our Location<span>3783 Armbrester Drive,USA</span></h4>
+</div>
+</div>
+
+</div>
+</div>
+</div>
+</div>
+
+
+<div class="header-menu">
+<div class="container">
+<div class="row">
+<div class="col-12">
+<nav class="navbar navbar-default">
+<div class="navbar-collapse">
+
+<ul id="nav" class="nav menu navbar-nav">
+<li class="active"><a href="index-2.html">Home<i class="far fa-angle-down"></i></a>
+<ul class="dropdown">
+<li><a href="index-2.html">Home Demo One</a></li>
+<li><a href="index-3.html">Home Demo Two</a></li>
+<li><a href="index-4.html">Home Demo Three</a></li>
+</ul>
+</li>
+<li><a href="#">Pages<i class="far fa-angle-down"></i></a>
+<ul class="dropdown">
+<li><a href="about.html">About Us</a></li>
+<li><a href="mission.html">Our Mission</a></li>
+<li><a href="#">Teachers<i class="far fa-angle-right"></i></a>
+<ul class="dropdown submenu">
+<li><a href="teacher.html">Teachers One</a></li>
+<li><a href="teacher-2.html">Teachers Two</a></li>
+<li><a href="teacher-single.html">Teacher Single</a></li>
+</ul>
+</li>
+<li><a href="gallery.html">Gallery</a></li>
+<li><a href="contact.html">Contact Us</a></li>
+<li><a href="testimonials.html">Testimonials</a></li>
+<li><a href="terms-conditions.html">Terms & Conditions</a></li>
+<li><a href="privacy-policy.html">Privacy Policy</a></li>
+<li><a href="faq.html">Faq</a></li>
+<li><a href="404.html">404 Page</a></li>
+</ul>
+</li>
+<li><a href="#">Courses<i class="far fa-angle-down"></i></a>
+<ul class="dropdown">
+<li><a href="courses.html">Courses</a></li>
+<li><a href="course-single.html">Course Single</a></li>
+<li><a href="course-single-video.html">Course Single Video</a></li>
+</ul>
+</li>
+<li><a href="#">Events<i class="far fa-angle-down"></i></a>
+<ul class="dropdown">
+<li><a href="events.html">Events</a></li>
+<li><a href="event-single.html">Event Single</a></li>
+</ul>
+</li>
+<li><a href="#">Blogs<i class="far fa-angle-down"></i></a>
+<ul class="dropdown">
+<li><a href="blogs.html">Blogs Grid Layout</a></li>
+<li><a href="blogs-left-sidebar.html">Blogs Left Sidebar</a></li>
+<li><a href="blogs-right-sidebar.html">Blogs Right Sidebar</a></li>
+<li><a href="blog-single-left-sidebar.html">Blog Single Left Sidebar</a></li>
+<li><a href="blog-single-right-sidebar.html">Blog Single Right Sidebar</a></li>
+</ul>
+</li>
+<li><a href="contact.html">Contact</a></li>
+</ul>
+
+
+<div class="button">
+<a href="contact.html" class="btn"><i class="far fa-pencil"></i>Apply Now</a>
+</div>
+
+</div>
+</nav>
+</div>
+</div>
+</div>
+</div>
+
+</header>
+
+
+<section class="breadcrumbs overlay">
+<div class="container">
+<div class="row">
+<div class="col-12">
+<h2>Event Single</h2>
+<ul class="bread-list">
+<li><a href="index-2.html">Home<i class="far fa-angle-right"></i></a></li>
+<li class="active"><a href="#">Event Single</a></li>
+</ul>
+</div>
+</div>
+</div>
+</section>
+
+
+<section class="events single section">
+<div class="container">
+<div class="row">
+<div class="col-lg-8 col-12">
+<div class="single-event">
+<div class="single-event-img">
+<img src="assets/img/events/2.jpg" alt="#">
+</div>
+<div class="event-content">
+<div class="meta">
+<span><i class="far fa-calendar-alt"></i>02 July 2021</span>
+<span><i class="far fa-clock"></i>10.00Am-4.00PM</span>
+<span><i class="far fa-map-marker-alt"></i>New York, USA</span>
+</div>
+<h2><a href="event-single.html">Foundation Program 2021</a></h2>
+<p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best.</p>
+<div class="row event-inner-img">
+<div class="col-lg-6">
+<img src="assets/img/events/6.jpg" alt="">
+</div>
+<div class="col-lg-6">
+<img src="assets/img/events/5.jpg" alt="">
+</div>
+</div>
+<p> Every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains.</p>
+<div class="book-now">
+<div class="button">
+<a href="#" class="btn">Book Now</a>
+</div>
+<ul class="social">
+<li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+<li><a href="#"><i class="fab fa-twitter"></i></a></li>
+<li><a href="#"><i class="fab fa-instagram"></i></a></li>
+<li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+<div class="col-lg-4 col-12">
+<div class="blog-sidebar">
+<div class="search">
+<div class="form">
+<input type="email" placeholder="Search Here...">
+<button class="button" href="#"><i class="far fa-search"></i></button>
+</div>
+</div>
+
+<div class="single-widget categories">
+<h3 class="title">Categories</h3>
+<ul>
+<li><a href="#"><i class="far fa-angle-double-right"></i>Online Course<span>15</span></a></li>
+<li><a href="#"><i class="far fa-angle-double-right"></i>Basic Knowledge<span>25</span></a></li>
+ <li><a href="#"><i class="far fa-angle-double-right"></i>Improve Skills<span>50</span></a></li>
+<li><a href="#"><i class="far fa-angle-double-right"></i>Proffesionals Course<span>22</span></a></li>
+<li><a href="#"><i class="far fa-angle-double-right"></i>Complete Course<span>29</span></a></li>
+</ul>
+</div>
+
+<div class="single-widget course">
+<h3>Popular Courses</h3>
+
+<div class="single-course">
+<img src="assets/img/course/1.jpg" alt="#">
+<div class="course-content">
+<h4><a href="#">Civil Engineering Course</a></h4>
+<div class="meta">$500.00, <span><i class="far fa-clock"></i>4 Years</span></div>
+</div>
+</div>
+
+
+<div class="single-course">
+<img src="assets/img/course/2.jpg" alt="#">
+<div class="course-content">
+<h4><a href="#">Business Management Course</a></h4>
+<div class="meta">$350.00, <span><i class="far fa-clock"></i>2 Years</span></div>
+</div>
+</div>
+
+
+<div class="single-course">
+<img src="assets/img/course/3.jpg" alt="#">
+<div class="course-content">
+<h4><a href="#">Food Science Course</a></h4>
+<div class="meta">$200.00, <span><i class="far fa-clock"></i>1 Year</span></div>
+</div>
+</div>
+
+</div>
+<div class="single-widget tags">
+<h3>Recent Tags</h3>
+<ul>
+<li><a href="#">Course</a></li>
+<li><a href="#">Study</a></li>
+<li><a href="#">Scholarship</a></li>
+<li><a href="#">Online</a></li>
+<li><a href="#">Knowledge</a></li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+
+<footer class="footer overlay section">
+
+<div class="footer-top">
+<div class="container">
+<div class="row">
+<div class="col-lg-3 col-md-6 col-12">
+
+<div class="single-widget about">
+<div class="logo"><a href="#"><img src="assets/img/logo2.png" alt="#"></a></div>
+<p>We are many variations of passages of orem psum available but the majority have suffered alteration in some form by injected humour or randomised words which don't look even slightly believable.</p>
+<ul class="list">
+<li><i class="far fa-phone"></i>(+1) 123 456 7894</li>
+<li><i class="far fa-envelope"></i> <a href="https://themes.themewild.com/cdn-cgi/l/email-protection#1c75727a735c79647d716c7079327f7371"><span class="__cf_email__" data-cfemail="30595e565f705548515d405c551e535f5d">[email&#160;protected]</span></a></li>
+<li><i class="far fa-map-marker-alt"></i>3783 Armbrester Drive,USA</li>
+</ul>
+</div>
+
+</div>
+<div class="col-lg-3 col-md-6 col-12">
+
+<div class="single-widget useful-links">
+<h2>Important Links</h2>
+<ul>
+<li><a href="#"><i class="far fa-angle-double-right"></i>About Us</a></li>
+<li><a href="#"><i class="far fa-angle-double-right"></i>Support</a></li>
+<li><a href="#"><i class="far fa-angle-double-right"></i>Faq</a></li>
+<li><a href="#"><i class="far fa-angle-double-right"></i>Upcoming Events</a></li>
+<li><a href="#"><i class="far fa-angle-double-right"></i>Our Blogs</a></li>
+<li><a href="#"><i class="far fa-angle-double-right"></i>Terms & Conditions</a></li>
+<li><a href="#"><i class="far fa-angle-double-right"></i>Contact Us</a></li>
+<li><a href="#"><i class="far fa-angle-double-right"></i>Privacy Policy</a></li>
+</ul>
+</div>
+
+</div>
+<div class="col-lg-3 col-md-6 col-12">
+
+<div class="single-widget latest-news">
+<h2>Latest Posts</h2>
+<div class="news-inner">
+<div class="single-news">
+<img src="assets/img/blog/sm-1.jpg" alt="#">
+<h4><a href="blog-single.html">Nam libero tempore cum soluta nobis est</a></h4>
+<p><i class="far fa-calendar-alt"></i> July 02, 2021</p>
+</div>
+<div class="single-news">
+<img src="assets/img/blog/sm-2.jpg" alt="#">
+<h4><a href="blog-single.html">At vero et accusamus et iusto odio dignissimos</a></h4>
+<p><i class="far fa-calendar-alt"></i> July 02, 2021</p>
+</div>
+<div class="single-news">
+<img src="assets/img/blog/sm-3.jpg" alt="#">
+<h4><a href="blog-single.html">Sed perspiciatis unde omnis iste natus sit </a></h4>
+<p><i class="far fa-calendar-alt"></i> July 02, 2021</p>
+</div>
+</div>
+</div>
+
+</div>
+<div class="col-lg-3 col-md-6 col-12">
+
+<div class="single-widget newsletter">
+<h2>Newsletter</h2>
+<div class="mail">
+<p>Subscribe to our newsletter, Get the latest updates and news from us!</p>
+<div class="form">
+<input type="email" placeholder="Enter your email">
+<button class="button" type="submit"><i class="far fa-paper-plane"></i></button>
+</div>
+</div>
+</div>
+
+
+<ul class="social">
+<li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+<li><a href="#"><i class="fab fa-twitter"></i></a></li>
+<li><a href="#"><i class="fab fa-whatsapp"></i></a></li>
+<li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+<li><a href="#"><i class="fab fa-youtube"></i></a></li>
+</ul>
+
+</div>
+</div>
+</div>
+</div>
+
+
+<div class="footer-bottom">
+<div class="container">
+<div class="row">
+<div class="col-12">
+<div class="bottom-head">
+<div class="row">
+<div class="col-12">
+
+<div class="copyright">
+<p>© Copyright 2021 <a href="#">EDULIFE</a> All Rights Reserved.</p>
+</div>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+</footer>
+
+
+<script data-cfasync="false" src="../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="assets/js/jquery-3.6.0.min.js"></script>
+
+<script src="assets/js/bootstrap.bundle.min.js"></script>
+
+<script src="assets/js/jquery.stellar.min.js"></script>
+
+<script src="assets/js/facnybox.min.js"></script>
+
+<script src="assets/js/jquery.magnific-popup.min.js"></script>
+
+<script src="assets/js/circle-progress.min.js"></script>
+
+<script src="assets/js/owl.carousel.min.js"></script>
+
+<script src="assets/js/waypoints.min.js"></script>
+
+<script src="assets/js/slicknav.min.js"></script>
+
+<script src="assets/js/jquery.counterup.min.js"></script>
+
+<script src="assets/js/easing.min.js"></script>
+
+<script src="assets/js/wow.min.js"></script>
+
+<script src="assets/js/jquery.scrollUp.min.js"></script>
+
+<script src="assets/js/main.js"></script>
+</body>
+
+<!-- Mirrored from themes.themewild.com/edulife/event-single.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 23 Jan 2022 08:39:32 GMT -->
+</html>
